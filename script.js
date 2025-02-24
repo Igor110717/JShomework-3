22a60265ed10cb5ad65acd5ce9b783c0fce58832
Igor110717 <igorkotov110717@gmail.com>
2025-02-24 13:21:20 +0200

    Решает задание 2

--- script.js
+++ script.js
@@ -19,3 +19,15 @@
 // console.log(`${key} : ${user[key]}`);
 // }
 
+// Задание 2
+
+// const countProps = function(obj) {
+// return Object.keys(obj).length;
+// };
+
+// console.log(countProps({})); // 0
+
+// console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
