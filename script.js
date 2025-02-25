585ebac244cd369314baf9269ba2c29fa82f3571
Igor110717 <igorkotov110717@gmail.com>
2025-02-25 10:36:44 +0200

    Делает 5 задание

--- script.js
+++ script.js
@@ -99,3 +99,27 @@
 //     chelsy: 150,
 //   }),
 // ); // 400
+
+// Задание 5
+
+// const products = [
+//     { name: 'Радар', price: 1300, quantity: 4 },
+//     { name: 'Сканер', price: 2700, quantity: 3 },
+//     { name: 'Дроид', price: 400, quantity: 7 },
+//     { name: 'Захват', price: 1200, quantity: 2 },
+//   ];
+  
+// const getAllPropValues = function(arr, prop) {
+// const arrProperty = [];
+// for (const elmement of arr) {
+//   if (elmement[prop] !== undefined) {
+//   arrProperty.push(elmement[prop]);
+// }
+// }
+// return arrProperty
+//   };
+//   console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+// console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+// console.log(getAllPropValues(products, 'category')); // []
